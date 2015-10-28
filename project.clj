c34b3fc703e766e57b2a0596d01c4ea714300097
Vignesh Sarma K <vignesh.sarma@gmail.com>
2015-10-28 11:40:06 +0530

    compile java portion to be able to work with 1.7.

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
   :java-source-paths ["java-src"]
   :global-vars {*warn-on-reflection* true
                 *assert* true}
+  :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
   :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.10"]]}
              :c15 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :c16 {:dependencies [[org.clojure/clojure "1.6.0"]]}
